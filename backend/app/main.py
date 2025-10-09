9f60e75850ee17976995f504f03782e057020a9e
thesonukumar <sonuinfirad0299@gmail.com>
2025-10-10 03:08:02 +0530

    fixed CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Brainstorming Board API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
